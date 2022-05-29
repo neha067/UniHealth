25f008ab44debc91d5257d994d89ab3f636f325d
Neha <nehapriyaekka@gmail.com>
2022-05-29 19:14:21 +0530

    added-signup

--- mysite/home/urls.py
+++ mysite/home/urls.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from django.urls import path,include
 from . import views
 urlpatterns = [    #URL dispatching.....
-    path('', views.login, name='index'),  #calls the index function from view
+    path('', views.index, name='index'),  #calls the index function from view
     path('index',views.index, name= 'index'),  #calls about function from view
     path('login',views.login, name= 'login'),
-    path('register',views.register, name= 'register')
+    path('signup',views.signup, name= 'signup')
 ]
--- mysite/home/views.py
+++ mysite/home/views.py
@@ -1,7 +1,9 @@
+from email import message
 from multiprocessing import context
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
-
+from django.contrib.auth.models import User
+from django.contrib import messages
 def index(request):
     
     return render(request,'index.html')
@@ -11,7 +13,20 @@ def about(request):
     return HttpResponse("this is about....")
 
 def login(request):
+    
     return render(request, 'login.html') #context is sent to html doc....
 
-def register(request):
-    return render(request, 'register.html') 
+def signup(request):
+    if request.method =="POST":
+        username= request.POST.get('user')
+        email= request.POST.get('email')
+        pass1= request.POST.get('pass1')
+        pass2= request.POST.get('pass2') 
+
+        myuser = User.objects.create_user(username,email,pass1)
+        myuser.save()
+
+        messages.success(request,"Your account has been successfully created!")
+
+        return redirect('login')
+    return render(request, 'signup.html') 
