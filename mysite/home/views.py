4d944d5e0ca57f76d765e0255b315224c0efe526
Neha <nehapriyaekka@gmail.com>
2022-05-30 10:12:25 +0530

    added-login

--- mysite/home/views.py
+++ mysite/home/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.contrib.auth import authenticate
 def index(request):
     
     return render(request,'index.html')
@@ -13,15 +14,26 @@ def about(request):
     return HttpResponse("this is about....")
 
 def login(request):
-    
+    if request.method == 'POST':
+        username = request.POST['username']
+        pass1 = request.POST['pass1']
+        
+        user = authenticate(username=username, password=pass1) #inbuilt function
+        if user is not None:
+            login(request)
+            # messages.success(request, "Logged In Sucessfully!!")
+            return render(request, "index.html")
+        else:
+            messages.error(request, "Bad Credentials!!")
+            return redirect('app')
     return render(request, 'login.html') #context is sent to html doc....
 
 def signup(request):
     if request.method =="POST":
-        username= request.POST.get('user')
-        email= request.POST.get('email')
-        pass1= request.POST.get('pass1')
-        pass2= request.POST.get('pass2') 
+        username= request.POST['username']
+        email= request.POST['email']
+        pass1= request.POST['pass1']
+        pass2= request.POST['pass2']
 
         myuser = User.objects.create_user(username,email,pass1)
         myuser.save()
