0749c044ae1df3d77e50106a4250a9dabb23be1b
Neha <nehapriyaekka@gmail.com>
2022-05-31 10:53:25 +0530

    added-student-model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
 # Create your models here.
+
+class Student(models.Model):
+    student_id = models.AutoField #IntegerField that automatically increments according to available IDs
+    student_name = models.CharField(max_length=20)
+    email = models.EmailField
+    regNo = models.IntegerField(unique = True)
+    phone= models.IntegerField()
+    address = models.CharField(max_length=150)
+    age = models.IntegerField()
+    gender = models.CharField (max_length=10)
+    dob = models.DateField() 
+
+
